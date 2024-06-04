b8f6e037fae35d65a7d9cdd41636f1ee5692945f
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-06-04 12:46:31 -0600

    Get individual records from tables

--- users-api/users/database.go
+++ users-api/users/database.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"errors"
+	"log"
 	"users/users/models"
 
 	"gorm.io/driver/sqlite"
@@ -20,4 +22,35 @@ func ConnectToDatabase() {
 		&models.Following{},
 	)
 	Database = database
+}
+
+func GetUserRecord(user_id string) (*models.User, error) {
+	var record models.User
+	result := Database.Where("user.id = ?", user_id).First(&record)
+	if result.Error != nil {
+		log.Println("Error occurred during query: " + result.Error.Error())
+		return nil, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return nil, errors.New("record not found in database")
+	}
+
+	return &record, nil
+}
+
+func GetFollowingRecord(follower_id, followed_id string) (*models.Following, error) {
+	var record models.Following
+	result := Database.
+		Where("user_following.follower_id = ?", follower_id).
+		Where("user_following.followed_id = ?", followed_id).
+		First(&record)
+	if result.Error != nil {
+		log.Println("Error occurred during query: " + result.Error.Error())
+		return nil, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return nil, errors.New("record not found in database")
+	}
+
+	return &record, nil
 }
