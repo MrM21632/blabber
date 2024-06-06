bf82959ad04bcaf76031afb92b407ad8a7d458eb
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-06-05 20:12:06 -0400

    Set constraints on user handle and email

--- users-api/users/models/user.go
+++ users-api/users/models/user.go
@@ -4,9 +4,9 @@ import "time"
 
 type User struct {
 	ID        string    `json:"id" gorm:"primaryKey;autoincrement:false"`
-	Handle    string    `json:"handle"`
+	Handle    string    `json:"handle" gorm:"unique;not null"`
 	Username  string    `json:"username"`
-	Email     string    `json:"email_address"`
+	Email     string    `json:"email_address" gorm:"unique;not null"`
 	Password  string    `json:"password_hash"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
