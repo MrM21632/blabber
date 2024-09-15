00a62f9d29e3cfdda50d0c45741b1edd7ed3b590
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-09-14 20:21:30 -0400

    Logging middleware, setting up the server

---
+++ users-api/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"users-api/middleware"
+	"users-api/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetLevel(log.InfoLevel)
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func main() {
+	godotenv.Load()
+
+	server_port := utils.GetServerPort()
+	PasswordParams := utils.GetArgon2IDConfig()
+	UIDGenerator, err := utils.InitializeNode()
+	if err != nil {
+		log.Error("encountered error while initializing uidgen node: " + err.Error())
+		return
+	}
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(middleware.LoggingMiddleware())
+	r.SetTrustedProxies(nil)
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"code": "ENDPOINT_NOT_FOUND", "message": "endpoint not found"})
+	})
+
+	r.Run(fmt.Sprintf(":%d", server_port))
+}
