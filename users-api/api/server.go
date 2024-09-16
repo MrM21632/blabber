975cb25943eedb3f43f16155c6f20633422cc2a0
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-09-15 20:08:48 -0400

    JSON parsing, user creation and retrieval endpoints

--- users-api/api/server.go
+++ users-api/api/server.go
@@ -1,9 +1,14 @@
 package api
 
 import (
+	"net/http"
+	"time"
+	"users-api/models"
 	"users-api/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,10 +18,111 @@ type UserServer struct {
 }
 
 // POST /users
-func (u UserServer) CreateUser(context *gin.Context) {}
+func (u UserServer) CreateUser(context *gin.Context) {
+	var err error
+
+	var input models.CreateUserRequest
+	if err = context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var new_user_id uuid.UUID
+	if new_user_id, err = uuid.NewV7(); err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "error occurred creating new user id: " + err.Error()},
+		)
+		return
+	}
+
+	var password_hash string
+	if password_hash, err = utils.GenerateHash(input.Password, u.Argon2Params); err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "error occurred hashing password: " + err.Error()},
+		)
+		return
+	}
+
+	query_string := `
+	INSERT INTO blabber.user (
+		id, username, user_handle, user_bio, email, password_hash, created_at, updated_at,
+		followers, follows
+	) VALUES (
+		@id, @username, @handle, @bio, @email, @hash, @createdAt, @updatedAt, @followers,
+		@follows
+	);
+	`
+	query_args := pgx.NamedArgs{
+		"id":        new_user_id,
+		"username":  input.Username,
+		"handle":    input.Handle,
+		"bio":       *input.Bio,
+		"email":     input.Email,
+		"hash":      password_hash,
+		"createdAt": time.Now(),
+		"updatedAt": time.Now(),
+		"followers": 0,
+		"follows":   0,
+	}
+
+	_, err = u.DatabasePool.Exec(context, query_string, query_args)
+	if err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "error occurred writing new user record: " + err.Error()},
+		)
+		return
+	}
+
+	context.JSON(
+		http.StatusCreated,
+		gin.H{"user_id": new_user_id},
+	)
+}
 
 // GET /users
-func (u UserServer) GetUser(context *gin.Context) {}
+func (u UserServer) GetUser(context *gin.Context) {
+	var err error
+
+	var input models.IndividualUserRequest
+	if err = context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	query_string := `
+	SELECT * FROM blabber.user
+	WHERE id = @id;
+	`
+	query_args := pgx.NamedArgs{
+		"id": input.ID,
+	}
+	row, err := u.DatabasePool.Query(context, query_string, query_args)
+	if err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "error occurred retrieving user record: " + err.Error()},
+		)
+		return
+	}
+	defer row.Close()
+
+	user, err := pgx.CollectOneRow(row, pgx.RowToStructByName[models.User])
+	if err != nil {
+		context.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "error occurred retrieving user record: " + err.Error()},
+		)
+		return
+	}
+
+	context.JSON(
+		http.StatusOK,
+		gin.H{"user": user},
+	)
+}
 
 // GET /users/followers
 func (u UserServer) GetFollowers(context *gin.Context) {}
--- users-api/models/database.go
+++ users-api/models/database.go
@@ -7,16 +7,16 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `db:"id"`
-	Username     string    `db:"username"`
-	UserHandle   string    `db:"user_handle"`
-	Bio          string    `db:"user_bio"`
-	Email        string    `db:"email"`
-	PasswordHash string    `db:"password_hash"`
-	CreatedAt    time.Time `db:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at"`
-	Followers    uint32    `db:"followers"`
-	Follows      uint32    `db:"follows"`
+	ID           uuid.UUID `db:"id" json:"user_id"`
+	Username     string    `db:"username" json:"username"`
+	Handle       string    `db:"user_handle" json:"user_handle"`
+	Bio          string    `db:"user_bio" json:"user_bio"`
+	Email        string    `db:"email" json:"email_address"`
+	PasswordHash string    `db:"password_hash" json:"password_hash"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
+	Followers    uint32    `db:"followers" json:"followers"`
+	Follows      uint32    `db:"follows" json:"follows"`
 }
 
 func (User) TableName() string {
@@ -24,9 +24,9 @@ func (User) TableName() string {
 }
 
 type UserFollow struct {
-	FollowerID uuid.UUID `db:"follower_id"`
-	FollowedID uuid.UUID `db:"followed_id"`
-	CreatedAt  time.Time `db:"created_at"`
+	FollowerID uuid.UUID `db:"follower_id" json:"follower_id"`
+	FollowedID uuid.UUID `db:"followed_id" json:"followed_id"`
+	CreatedAt  time.Time `db:"created_at" json:"created_at"`
 }
 
 func (UserFollow) TableName() string {
@@ -34,9 +34,9 @@ func (UserFollow) TableName() string {
 }
 
 type UserBlock struct {
-	BlockerID uuid.UUID `db:"blocker_id"`
-	BlockedID uuid.UUID `db:"blocked_id"`
-	CreatedAt time.Time `db:"created_at"`
+	BlockerID uuid.UUID `db:"blocker_id" json:"blocker_id"`
+	BlockedID uuid.UUID `db:"blocked_id" json:"blocked_id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
 
 func (UserBlock) TableName() string {
@@ -44,9 +44,9 @@ func (UserBlock) TableName() string {
 }
 
 type UserMute struct {
-	MuterID   uuid.UUID `db:"muter_id"`
-	MutedID   uuid.UUID `db:"muted_id"`
-	CreatedAt time.Time `db:"created_at"`
+	MuterID   uuid.UUID `db:"muter_id" json:"muter_id"`
+	MutedID   uuid.UUID `db:"muted_id" json:"muted_id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
 
 func (UserMute) TableName() string {
