6948dcb9492e5e915cc640cff96f4b4c96795e39
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-10-05 23:42:59 -0400

    Update create script to also insert test data

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -1,3 +1,9 @@
 # Uncomment if the database also doesn't exist
 # createdb -U blabber blabber
+
+# Create database schema and tables
 psql -U blabber -d blabber -a -f ./blabber.sql
+
+# Populate tables
+psql -U blabber -d blabber -a -f ./blabber_users.sql
+psql -U blabber -d blabber -a -f ./blabber_posts.sql
